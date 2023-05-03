d7feafb1c815169a285c909ef85523319cf6ac72
Cameron Gray <cameron@procuros.io>
2023-05-03 13:09:48 +0100

    started on book cipher

---
+++ book/book.js
@@ -0,0 +1,58 @@
+function findAllIndexes (letter, string) {
+    const positions = [];
+
+    const findNextLetter = (letter, startingPos) => {
+        if (startingPos >= string.length) {
+            return;
+        }
+
+        const position = string.indexOf(letter, startingPos);
+
+        if (position >= 0) {
+            positions.push(position);
+            findNextLetter(letter, position + 1);
+        }
+
+        return;
+    }
+    
+    findNextLetter(letter, string);
+
+    return positions;
+}
+
+function bookCipher(passage, message) {
+    const passageByLines = passage.split("\n");
+    let encryptedMessage = "";
+
+    message.split("").forEach(letter => {
+        const letterExists = passage.split("").findIndex(passageLetter => letter === passageLetter);
+        if (letterExists < 0) {
+            encryptedMessage += " " + letter;
+            return;
+        }
+
+        const allPositionsOfLetter = [];
+
+        passageByLines.forEach((line, lineIndex) => allPositionsOfLetter.push(findAllIndexes(letter, line).map(index => `${lineIndex}:${index}`)));
+        
+        if (!allPositionsOfLetter.length) {
+            encryptedMessage += letter;
+            return;
+        }
+
+        const randomLine = Math.floor(Math.random() * allPositionsOfLetter.length);
+        const randomPositionIndex = Math.floor(Math.random() * allPositionsOfLetter[randomLine].length);
+        const randomPosition = allPositionsOfLetter[randomLine][randomPositionIndex];
+        
+        encryptedMessage += " " + randomPosition;
+    });
+    
+    return encryptedMessage;
+}
+
+const passage = 'something is happening. Test test. \nThe quick brown fox jumped over the lazy dog.';
+const message = 'cameron';
+const encryptedMessage = bookCipher(passage, message);
+
+console.log(encryptedMessage);
